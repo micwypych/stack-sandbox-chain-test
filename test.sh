6d6ec5d44b8d5293fb12cc94156f32403ff6603e
Michał Wypych <mwypych@agh.edu.pl>
2018-08-21 18:01:32 +0200

    Commits failed try to make it work

--- test.sh
+++ test.sh
@@ -1,19 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
 STACK=${1:-stack}
 
 pushd lib1
+echo "changing working directory to: "
+pwd
 ${STACK} clean
-${STACK} build
+${STACK} --resolver lts-12.7 build
 popd
 
 pushd lib2
+echo "changing working directory to: "
+pwd
 ${STACK} clean
-${STACK} build
+${STACK} --resolver lts-12.7 solver
+${STACK} --resolver lts-12.7 build
 popd
 
 pushd app
+echo "changing working directory to: "
+pwd
 ${STACK} clean
-${STACK} build
-${STACK} exec app
+${STACK} --resolver lts-12.7 solver
+${STACK} --resolver lts-12.7 build
+${STACK} --resolver lts-12.7 exec app
 popd
